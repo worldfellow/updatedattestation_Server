a626cc35ceaa6138655a2bc95a4a609d1229ce7d
Karthik-edulab <karthik@edulab.in>
2023-06-28 17:06:51 +0530

    KG-Feat-letter_details table created

---
+++ models/letter_details.js
@@ -0,0 +1,23 @@
+
+"use strict"
+
+module.exports = function(sequelize, DataTypes) {
+var letterDetails = sequelize.define("letter_details", {
+    user_id: DataTypes.INTEGER,
+    studentName: DataTypes.STRING(255),
+    courseName: DataTypes.STRING(255),
+    collegeName : DataTypes.STRING(255),
+    specialization: DataTypes.STRING(255), 
+    yearofpassing: DataTypes.STRING(255),
+    duration: DataTypes.INTEGER,
+    division: DataTypes.STRING(255), 
+    education_type: DataTypes.STRING(255),
+    faculty : DataTypes.STRING(255),
+    type : DataTypes.STRING(255),
+    app_id : DataTypes.INTEGER
+ });
+
+ letterDetails.belongsTo(sequelize.models.Application, {foreignKey: 'app_id'});
+
+ return letterDetails;
+};
